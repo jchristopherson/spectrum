3deded5437a184731acf8392670b0bd0460431c5
jchristopherson <jchristopherson@hotmail.com>
2023-03-17 13:36:47 -0500

    Add tests

---
+++ test/spectrum_psd_tests.f90
@@ -0,0 +1,55 @@
+module spectrum_psd_tests
+    use iso_fortran_env
+    use spectrum
+    use fortran_test_helper
+    implicit none
+
+contains
+
+function test_psd() result(rst)
+    ! Arguments
+    logical :: rst
+
+    ! Local Variables
+    real(real64), parameter :: tol = 1.0d-6
+    integer(int32), parameter :: npts = 10000
+    integer(int32), parameter :: winsize = 1024
+    real(real64), parameter :: sample_rate_hz = 1.024d3
+    real(real64), parameter :: pi = 2.0d0 * acos(0.0d0)
+    real(real64), parameter :: freq1 = 2.5d1
+    real(real64), parameter :: amp1 = 2.0d0
+    integer(int32) :: i
+    real(real64) :: t, dt, ans, df, p
+    real(real64) :: x(npts)
+    real(real64), allocatable :: pwr(:)
+    type(hann_window) :: win
+
+    ! Initialization
+    rst = .true.
+    win%size = winsize
+
+    ! Construct a sinusoidal signal
+    t = 0.0d0
+    dt = 1.0d0 / sample_rate_hz
+    x(1) = 0.0d0
+    do i = 2, npts
+        t = t + dt
+        x(i) = amp1 * sin(2.0d0 * pi * freq1 * t)
+    end do
+
+    ! Compute the power spectrum
+    pwr = psd(win, x, sample_rate_hz)
+
+    ! Compute the expected solution
+    df = frequency_bin_width(sample_rate_hz, winsize)
+    ans = amp1**2 / df
+
+    ! Test
+    p = maxval(pwr)
+    if (.not.assert(p, ans, tol)) then
+        rst = .false.
+        print '(A)', "TEST FAILED: test_psd 1-1"
+    end if
+end function
+
+end module
--- test/spectrum_tests.f90
+++ test/spectrum_tests.f90
@@ -1,3 +1,18 @@
 program test
+    use spectrum_psd_tests
     implicit none
+
+    ! Local Variables
+    integer(int32) :: flag
+    logical :: local
+
+    ! Initialization
+    flag = 0
+
+    ! Tests
+    local = test_psd()
+    if (.not.local) flag = 1
+
+    ! Output
+    stop flag
 end program
