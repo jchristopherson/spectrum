8019800251d9c13018f518626086ddb7916df0ee
jchristopherson <jchristopherson@hotmail.com>
2023-03-31 05:38:17 -0500

    Clean up code

--- src/spectrum_fft.f90
+++ src/spectrum_fft.f90
@@ -1,19 +1,6 @@
 submodule (spectrum) spectrum_fft
     use fftpack
 contains
-! REFERENCES:
-! - https://en.wikipedia.org/wiki/Short-time_Fourier_transform
-! - https://en.wikipedia.org/wiki/Overlap%E2%80%93add_method
-!
-! Overlap-Add References:
-! - https://ccrma.stanford.edu/~jos/sasp/Example_Overlap_Add_Convolution.html
-! - https://www.mathworks.com/matlabcentral/fileexchange/41338-block-convolution-using-overlap-add-method
-! - https://github.com/vishwas1234567/Octave-scilab/blob/master/ovelapadd%20and%20ovelap%20save%20in%20matlab.m
-
-! Other Options:
-! - Frequency Domain Windowing:
-!   - https://www.embedded.com/dsp-tricks-frequency-domain-windowing/
-
 
 module function stft(win, x, offsets, par, err) result(rst)
     ! Arguments
