bc9f8010a297d71496c29dedbb71d468b5672414
jchristopherson <jchristopherson@hotmail.com>
2023-04-12 19:12:02 +0000

    Deploying to gh-pages from @ jchristopherson/spectrum@7269ff692697f8354f1072de4ed91e6be52734e2 🚀

---
+++ search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "bcefghimnoprstuw",
+  1: "bcfghimnoprstuw",
+  2: "e"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions"
+};
+
