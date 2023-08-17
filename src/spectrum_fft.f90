4df4208a71881f353211cb7bf4de4cebdd59e0f7
jchristopherson <jchristopherson@hotmail.com>
2023-08-17 16:11:32 -0500

    Update parallel do loop

--- src/spectrum_fft.f90
+++ src/spectrum_fft.f90
@@ -1,21 +1,19 @@
 submodule (spectrum) spectrum_fft
     use fftpack
 contains
-
-module function stft(win, x, offsets, par, err) result(rst)
+! ------------------------------------------------------------------------------
+module function stft(win, x, offsets, err) result(rst)
     ! Arguments
     class(window), intent(in) :: win
     real(real64), intent(in) :: x(:)
     integer(int32), intent(out), optional, allocatable :: offsets(:)
-    logical, intent(in), optional :: par
     class(errors), intent(inout), optional, target :: err
     complex(real64), allocatable :: rst(:,:)
 
     ! Local Variables
-    logical :: p
-    integer(int32) :: i, j, k, m, nx, nxfrm, nend, nk, lwork, flag, i1
-    real(real64) :: fac, w, sumw, del, scale
-    real(real64), allocatable :: work(:), buffer(:,:)
+    integer(int32) :: i, m, nx, nxfrm, nk, lwork, flag, i1
+    real(real64) :: del
+    real(real64), allocatable :: work(:)
     class(errors), pointer :: errmgr
     type(errors), target :: deferr
     character(len = :), allocatable :: errmsg
@@ -29,13 +27,6 @@ module function stft(win, x, offsets, par, err) result(rst)
     nx = size(x)
     m = win%size          ! # of rows in the output matrix (transform length)
     nxfrm = compute_transform_length(m)
-    if (mod(m, 2) == 0) then
-        nend = nxfrm - 1
-        scale = 2.0d0 / m
-    else
-        nend = nxfrm
-        scale = 2.0d0 / (m - 1.0d0)
-    end if
     lwork = 2 * m + 15
     nk = compute_overlap_segment_count(nx, m)
     if (nk > 1) then
@@ -43,22 +34,12 @@ module function stft(win, x, offsets, par, err) result(rst)
     else
         del = 0.0d0
     end if
-    if (present(par)) then
-        p = par
-    else
-        if (nk > 50) then
-            p = .true.
-        else
-            p = .false.
-        end if
-    end if
 
     ! Input Checking
     if (m > nx) go to 20
 
     ! Memory Allocation
     allocate(rst(nxfrm, nk), stat = flag)
-    if (flag == 0) allocate(buffer(m, nk), stat = flag, source = 0.0d0)
     if (flag == 0) allocate(work(lwork), stat = flag)
     if (present(offsets)) then
         if (flag == 0) allocate(offsets(nk), stat = flag, source = 0)
@@ -69,70 +50,18 @@ module function stft(win, x, offsets, par, err) result(rst)
     ! same length, this array can be shared.
     call dffti(m, work)
 
-    ! Compute each transform
-    if (p) then
-!$OMP PARALLEL PRIVATE(i1, j, k, w, sumw, fac)
-!$OMP DO
+    ! Store offsets, if necessary
+    if (present(offsets)) then
         do i = 1, nk
-            ! Locate the portion of the signal on which to operate
             i1 = int((i - 1) * del + 0.5d0, int32)
-            if (present(offsets)) offsets(i) = i1 + 1
-
-            ! Apply the window
-            j = 0
-            sumw = 0.0d0
-            do k = 1, m
-                w = win%evaluate(j)
-                j = j + 1
-                sumw = sumw + w
-                buffer(k,i) = w * x(i1+k)
-            end do
-            fac = m / sumw
-
-            ! Compute the transform
-            call dfftf(m, buffer(:,i), work)
-
-            ! Scale the transform
-            rst(1,i) = fac * scale * cmplx(buffer(1,i), 0.0d0, real64)
-            do k = 2, nend
-                rst(k,i) = fac * scale * cmplx(buffer(2*k-1,i), &
-                    buffer(2*k,i), real64)
-            end do
-            if (nend /= nxfrm) rst(nxfrm,i) = fac * scale * &
-                cmplx(buffer(m,i), 0.0d0, real64)
-        end do
-!$OMP END DO
-!$OMP END PARALLEL
-    else
-        do i = 1, nk
-            ! Locate the portion of the signal on which to operate
-            i1 = int((i - 1) * del + 0.5d0, int32)
-            if (present(offsets)) offsets(i) = i1 + 1
-
-            ! Apply the window
-            j = 0
-            sumw = 0.0d0
-            do k = 1, m
-                w = win%evaluate(j)
-                j = j + 1
-                sumw = sumw + w
-                buffer(k,i) = w * x(i1+k)
-            end do
-            fac = m / sumw
-
-            ! Compute the transform
-            call dfftf(m, buffer(:,i), work)
-
-            ! Scale the transform
-            rst(1,i) = fac * scale * cmplx(buffer(1,i), 0.0d0, real64)
-            do k = 2, nend
-                rst(k,i) = fac * scale * cmplx(buffer(2*k-1,i), &
-                    buffer(2*k,i), real64)
-            end do
-            if (nend /= nxfrm) rst(nxfrm,i) = fac * scale * &
-                cmplx(buffer(m,i), 0.0d0, real64)
+            offsets(i) = i1
         end do
     end if
+
+    ! Compute each transform
+    do concurrent (i = 1:nk)
+        rst(:,i) = stft_core(win, x, i, del, work)
+    end do
 
     ! End
     return
@@ -158,4 +87,58 @@ module function stft(win, x, offsets, par, err) result(rst)
 101 format(A, I0, A, I0, A)
 end function
 
+! ------------------------------------------------------------------------------
+pure function stft_core(win, x, offset, del, trig) result(rst)
+    ! Arguments
+    class(window), intent(in) :: win
+    real(real64), intent(in), dimension(:) :: x
+    integer(int32), intent(in) :: offset
+    real(real64), intent(in) :: del
+    real(real64), intent(in), dimension(:) :: trig
+    complex(real64), allocatable, dimension(:) :: rst
+
+    ! Local Variables
+    integer(int32) :: j, k, m, nxfrm, nend, i1
+    real(real64) :: sumw, w, fac, scale
+    real(real64), allocatable, dimension(:) :: buffer
+
+    ! Initialization
+    m = win%size
+    nxfrm = compute_transform_length(m)
+    i1 = int((offset - 1) * del + 0.5d0, int32)
+    allocate(buffer(m))
+    allocate(rst(nxfrm))
+    if (mod(m, 2) == 0) then
+        nend = nxfrm - 1
+        scale = 2.0d0 / m
+    else
+        nend = nxfrm
+        scale = 2.0d0 / (m - 1.0d0)
+    end if
+    
+    ! Apply the window
+    sumw = 0.0d0
+    j = 0
+    do k = 1, m
+        w = win%evaluate(j)
+        j = j + 1
+        sumw = sumw + w
+        buffer(k) = w * x(k + i1)
+    end do
+    fac = m / sumw
+
+    ! Compute the transform
+    call dfftf(m, buffer, trig)
+
+    ! Scale the transform
+    rst(1) = fac * scale * cmplx(buffer(1), 0.0d0, real64)
+    do k = 2, nend
+        rst(k) = fac * scale * cmplx(buffer(2*k-1), buffer(2*k), real64)
+    end do
+    if (nend /= nxfrm) then
+        rst(nxfrm) = fac * scale * cmplx(buffer(m), 0.0d0, real64)
+    end if
+end function
+
+! ------------------------------------------------------------------------------
 end submodule
